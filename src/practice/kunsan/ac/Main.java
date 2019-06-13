80c5e3efb0650d87cb41bbab918489b8053bc063
jhp0303 <47491887+jhp0303@users.noreply.github.com>
2019-06-13 16:19:17 +0900

    Practice #1 - 온도변화

--- src/practice/kunsan/ac/Main.java
+++ src/practice/kunsan/ac/Main.java
@@ -3,9 +3,9 @@ package practice.kunsan.ac;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Time is 2:42 PM");
-		System.out.println("기분좋은 낮이에요");
+		//System.out.println("Hello World");
+		
+		PracticeClass.practice1();
 
 	}
 
---
+++ src/practice/kunsan/ac/PracticeClass.java
@@ -0,0 +1,12 @@
+package practice.kunsan.ac;
+
+public class PracticeClass {
+
+	public static void practice1()	{	//¼·¾¾ ¿Âµµ¸¦ È­¾¾ ¿Âµµ·Î º¯È¯
+		double c = 31.5;
+		System.out.println("¼·¾¾: " + c );
+		double f = (c * 9/5) + 32;
+		System.out.println("È­¾¾: " + f );
+	}
+
+}
