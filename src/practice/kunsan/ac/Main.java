c0ec02e0d1697a5f9fb213826aa0829003babed0
jhp0303 <47491887+jhp0303@users.noreply.github.com>
2019-06-13 16:01:58 +0900

    출력 추가

--- src/practice/kunsan/ac/Main.java
+++ src/practice/kunsan/ac/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Time is 2:42 PM");
-		
+		System.out.println("기분좋은 낮이에요");
 
 	}
 
